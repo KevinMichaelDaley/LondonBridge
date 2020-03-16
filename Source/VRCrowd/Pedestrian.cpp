af352706f368436f916b937d0b659dfb2778cb03
Kevin Daley <kdaley3@gsu.edu>
2020-03-16 18:23:27 -0400

    works with dummy but i dont like

--- Source/VRCrowd/Pedestrian.cpp
+++ Source/VRCrowd/Pedestrian.cpp
@@ -83,17 +83,17 @@ void APedestrian::Tick(float DeltaTime) {
 // a=std::sqrt(std::sin(a*M_PI/2));
  float L=COMDistanceFromFoot_L;
  float p=VerticalStepWidth_p;
-  COMVertical = (L*100-std::cos(std::fmod(y[0]-p,p))*L*100);
+//  COMVertical = a*Forward;
   float tnext=TimeNextStep_tnext;
   COMLateral = (y[2]-u)*-100;
   COMSagittal = std::sin(std::fmod(y[0]-p,p))*L*100*ForwardSpeed;
-
+COMVertical = (L*100-std::cos(std::fmod(y[0]-p,p))*L*100);
 //  COMVertical = StepHeight*std::sin(a)
 //    float a=std::sin(y[1]
     float Omegap=std::sqrt(9.81/COMDistanceFromFoot_L);
     float bmin=(1-2*WhichFoot)*LateralControlWidth_bmin;
     float u2=(y[3]/Omegap+bmin+y[2]-u0)*-100;
-  float s0 =(2*b-1)*ForwardSpeed*L*100-COMSagittal;
+  float s0 =(1-std::cos(b*M_PI))*ForwardSpeed*L*100-COMSagittal;
 
   float u02=StanceFoot0.X-GetActorLocation().X;
   if (WhichFoot) {
